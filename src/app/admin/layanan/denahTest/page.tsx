cf6d0fee59c6eea33781657b8c41fe30b7498de0
Radho <radhofanazizi@gmail.com>
2025-07-06 13:26:25 +0700

    ada

--- src/app/admin/layanan/denahTest/page.tsx
+++ src/app/admin/layanan/denahTest/page.tsx
@@ -12,13 +12,35 @@ export default function Denah() {
   return (
     <div className="min-h-screen flex flex-col">
       <Header hideBanner />
+      <div className="text-3xl font-bold text-center mb-4">
+        Status Pemakaman
+      </div>
+      <main className="flex-grow container mx-auto px-4 py-10">
+        <h1 className="text-2xl font-bold mb-6 text-left">Pilih Kavling</h1>
+        <div className="flex justify-between items-center mb-6">
+          <div>
+            <label htmlFor="graveyard-select" className="mr-2 font-semibold">
+              Pilih Daerah:
+            </label>
+            <select
+              id="graveyard-select"
+              defaultValue="Dalem Kaum"
+              className="border border-gray-300 rounded-md p-1"
+            >
+              <option>Dalem Kaum</option>
+              <option>Dayeuhkolot</option>
+              <option>Karang Anyar</option>
+            </select>
+          </div>
+        </div>
         <div onClick={handleClick}>
-          <object
+        <object
             data="/images/Denah.svg"
             type="image/svg+xml"
             className="w-full h-auto"
           />
         </div>
+      </main>
       <Footer />
     </div>
   );
