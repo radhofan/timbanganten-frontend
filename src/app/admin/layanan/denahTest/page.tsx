088fa279c5e431705b590b696e0135fcc54a9acd
Radho <radhofanazizi@gmail.com>
2025-07-06 13:23:13 +0700

    ada

--- src/app/admin/layanan/denahTest/page.tsx
+++ src/app/admin/layanan/denahTest/page.tsx
@@ -1,17 +1,12 @@
 "use client";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { useRouter } from 'next/router'
 
 export default function Denah() {
-  const router = useRouter()
 
   const handleClick = (e: React.MouseEvent) => {
     const id = (e.target as HTMLElement).id
-    if (id?.startsWith('makam-')) {
-      const num = id.replace('makam-', '')
-      router.push(`/makam/${num}`)
-    }
+    console.log('Clicked ID:', id)
   }
 
   return (
