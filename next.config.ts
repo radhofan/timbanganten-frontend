7e529093a440be685940277a576496d199e5add7
Radho <radhofanazizi@gmail.com>
2025-08-15 21:47:05 +0700

    fix: guest login

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,19 @@ const nextConfig: NextConfig = {
     });
     return config;
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, max-age=0, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
