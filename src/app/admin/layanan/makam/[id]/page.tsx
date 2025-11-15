f1d4e7dafd79bdae232ce5d23fbf340c8e1e3074
Radho <radhofanazizi@gmail.com>
2025-11-15 15:34:51 +0700

    simpan tombol to edit

--- src/app/admin/layanan/makam/[id]/page.tsx
+++ src/app/admin/layanan/makam/[id]/page.tsx
@@ -393,7 +393,7 @@ export default function Edit() {
                     type="submit"
                     className="px-6 py-2 rounded-lg bg-blue-600 text-white font-medium hover:bg-blue-700 transition"
                   >
-                    Edit
+                    Simpan
                   </button>
                 </div>
               )}
